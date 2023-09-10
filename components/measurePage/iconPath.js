b2a75d6498d3cbac63e5b95948756af035137c90
MYPC\jisu0 <sbskjs54@gmail.com>
2023-09-10 22:13:00 +0900

    [Fix] playbutton두께수정 (#33)

--- components/measurePage/iconPath.js
+++ components/measurePage/iconPath.js
@@ -1,7 +1,7 @@
 export const iconPath = {
   arrowLeft: require('../../assets/images/arrowLeft.png'),
   arrowRight: require('../../assets/images/arrowRight.png'),
-  playButton: require('../../assets/images/play.png'),
-  pausedButton: require('../../assets/images/paused.png'),
+  playButton: require('../../assets/images/play-modified.png'),
+  pausedButton: require('../../assets/images/paused-modified.png'),
   newPlayButton: require('../../assets/images/newplaybutt.png'),
 };
