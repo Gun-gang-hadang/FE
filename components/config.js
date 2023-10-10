a95c9639c4a11b0211648ffde49d840f2926fdfb
genieu99 <yujin0419@sookmyung.ac.kr>
2023-10-10 23:15:06 +0900

    [Chore] 내혈당 삭제 알랏 개선 (#73)

--- components/config.js
+++ components/config.js
@@ -2,9 +2,6 @@ const config = {
     //proxyUrl: "http://10.0.2.2:8080",
     // proxyUrl: "http://와이파이:포트",
 
-    // 유진집
-    // proxyUrl: "http://172.30.1.41:8080",
-
     // 서버 배포 주소
     proxyUrl: "http://3.35.30.228:8080"
     
